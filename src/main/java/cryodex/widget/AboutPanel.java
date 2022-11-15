1746cfa2a322046b7d0a7f8c62bb9ebb8bf791d4
Marcos Gamez <marcos.gam@hotmail.com>
2022-11-15 15:27:18 +0100

    init JavaFx

--- src/main/java/cryodex/widget/AboutPanel.java
+++ src/main/java/cryodex/widget/AboutPanel.java
@@ -1,73 +1,58 @@
 package cryodex.widget;
 
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SpringLayout;
-
 import cryodex.Main;
+import javafx.embed.swing.JFXPanel;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
-/**
- * Basic panel for displaying information on how the program got started.
- * 
- * @author cbrown
- *
- */
-public class AboutPanel extends JPanel {
-
-	private static final long serialVersionUID = 1L;
-
-	private JButton closeButton;
-	private final JDialog parent;
+public class AboutPanel {
+	private Button closeButton;
+    static JDialog manualModificationPanel = new JDialog(Main.getInstance(),"About", true);
 
 	public static void showAboutPanel() {
-		JDialog manualModificationPanel = new JDialog(Main.getInstance(),
-				"About", true);
-		JPanel panel = new JPanel();
+
+		JFXPanel panel = new JFXPanel();
 		manualModificationPanel.getContentPane().add(panel);
-		panel.add(new AboutPanel(manualModificationPanel));
+		new AboutPanel(panel);
 		manualModificationPanel.setLocationRelativeTo(Main.getInstance());
 		manualModificationPanel.pack();
-
 		manualModificationPanel.setVisible(true);
 	}
 
-	private AboutPanel(JDialog parent) {
-		super(new SpringLayout());
+	private AboutPanel(JFXPanel panel) {
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(10);
+		String aboutText = "This program was created for the Campaign Against " +
+            "Cancer Tournament. A special thanks to Chad Hoefle and " +
+            "Anthony Lullig for their encouragement and testing during " +
+            "that time. I would also like to thank all of those who " +
+            "encouraged me to make it better and distribute it after that " +
+            "tournament was complete. My goal is to have a program that #1 " +
+            "follows the rules and #2 is easy to use. You are welcome to " +
+            "contact me with any comments or concerns you have about the " +
+            "program. My email is Chris.Brown.SPE@gmail.com. You can also " +
+            "use that email to send a donation via Paypal if you feel so " +
+            "inclined.";
 
-		this.parent = parent;
-
-		String aboutText = "<HTML>This program was created for the Campaign Against Cancer Tournament. A special thanks to Chad Hoefle and Anthony Lullig for their encouragement and testing during that time. I would also like to thank all of those who encouraged me to make it better and distribute it after that tournament was complete. My goal is to have a program that #1 follows the rules and #2 is easy to use. You are welcome to contact me with any comments or concerns you have about the program. My email is Chris.Brown.SPE@gmail.com. You can also use that email to send a donation via Paypal if you feel so inclined.</HTML>";
-
-		JLabel aboutLabel = new JLabel(aboutText);
-		ComponentUtils.forceSize(aboutLabel, 400, 175);
-
-		this.add(aboutLabel);
-
-		this.add(ComponentUtils.addToFlowLayout(getCloseButton(),
-				FlowLayout.CENTER));
-
-		SpringUtilities.makeCompactGrid(this, this.getComponentCount(), 1, 1,
-				1, 1, 1);
+		Label aboutLabel = new Label(aboutText);
+		root.getChildren().add(aboutLabel);
+        root.getChildren().add(getCloseButton());
+        panel.setScene(new Scene(root));
 	}
 
-	private JButton getCloseButton() {
+	private Button getCloseButton() {
 		if (closeButton == null) {
-			closeButton = new JButton("Close");
-
-			closeButton.addActionListener(new ActionListener() {
-
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					parent.setVisible(false);
-				}
-			});
-
+			closeButton = new Button("Close");
+			closeButton.addEventFilter(
+                    MouseEvent.MOUSE_CLICKED,
+                    MouseEvent -> manualModificationPanel.setVisible(false)
+			);
 		}
 		return closeButton;
 	}
