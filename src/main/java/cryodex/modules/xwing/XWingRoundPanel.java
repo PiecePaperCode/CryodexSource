e8bf85b0a0282fe80cc0143716f30ae69dcee30a
Marcos Gamez <dev@marcos-gamez.ch>
2022-11-21 16:41:18 +0100

    javafx XWingRoundPanel

--- src/main/java/cryodex/modules/xwing/XWingRoundPanel.java
+++ src/main/java/cryodex/modules/xwing/XWingRoundPanel.java
@@ -1,58 +1,29 @@
 package cryodex.modules.xwing;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import javax.swing.BorderFactory;
-import javax.swing.DefaultListCellRenderer;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-
 import cryodex.CryodexController;
-import cryodex.Player;
-import cryodex.widget.ComponentUtils;
-import cryodex.widget.ConfirmationTextField;
+import cryodex.components.Bootstrap;
+import cryodex.widget.JFXSwingPanel;
+import javafx.embed.swing.JFXPanel;
+import javafx.geometry.Insets;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
-public class XWingRoundPanel extends JPanel {
-
-	private static final long serialVersionUID = 1L;
-
+public class XWingRoundPanel {
 	private final List<XWingMatch> matches;
-	private final List<GamePanel> gamePanels = new ArrayList<GamePanel>();
-	private JPanel quickEntryPanel;
-	private JPanel quickEntrySubPanel;
-	private JTextField roundNumber;
-	private JComboBox<XWingPlayer> playerCombo;
-	private final JScrollPane scroll;
-
+	private final List<GamePanel> gamePanels = new ArrayList<>();
 	private final XWingTournament tournament;
+	private final JFXPanel panel;
 
 	public XWingRoundPanel(XWingTournament t, List<XWingMatch> matches) {
-
-		super(new BorderLayout());
-
 		this.tournament = t;
 		this.matches = matches;
-		this.setBorder(BorderFactory.createLineBorder(Color.black));
 
 		int counter = 1;
 		for (XWingMatch match : matches) {
@@ -61,272 +32,65 @@ public class XWingRoundPanel extends JPanel {
 			counter++;
 		}
 
-		scroll = new JScrollPane(ComponentUtils.addToFlowLayout(buildPanel(), FlowLayout.CENTER));
-		scroll.setBorder(BorderFactory.createEmptyBorder());
-
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				scroll.getVerticalScrollBar().setValue(0);
-				scroll.getVerticalScrollBar().setUnitIncrement(15);
-			}
-		});
-
-		this.add(getQuickEntryPanel(), BorderLayout.NORTH);
-		this.add(scroll, BorderLayout.CENTER);
+		ScrollPane scroll = new ScrollPane(buildPanel());
+		
+		var root = new Bootstrap().VBox(scroll);
+		panel = JFXSwingPanel.create(root);
 	}
 
-	public JPanel getQuickEntryPanel() {
-		if (quickEntryPanel == null) {
-			quickEntryPanel = new JPanel(new BorderLayout());
-			quickEntryPanel.setVisible(CryodexController.getOptions().isShowQuickFind());
-			ComponentUtils.forceSize(quickEntryPanel, 405, 135);
-
-			quickEntrySubPanel = new JPanel(new BorderLayout());
-			ComponentUtils.forceSize(quickEntrySubPanel, 400, 130);
-
-			roundNumber = new JTextField(5);
-
-			roundNumber.getDocument().addDocumentListener(new DocumentListener() {
-				@Override
-				public void changedUpdate(DocumentEvent e) {
-					update();
-				}
-
-				@Override
-				public void removeUpdate(DocumentEvent e) {
-					update();
-				}
-
-				@Override
-				public void insertUpdate(DocumentEvent e) {
-					update();
-				}
-			});
-
-			List<XWingPlayer> playerList = new ArrayList<XWingPlayer>();
-
-			playerList.add(new XWingPlayer(new Player()));
-			playerList.addAll(tournament.getXWingPlayers());
-
-			Collections.sort(playerList);
-
-			playerCombo = new JComboBox<XWingPlayer>(playerList.toArray(new XWingPlayer[playerList.size()]));
-
-			playerCombo.addActionListener(new ActionListener() {
-
-				@Override
-				public void actionPerformed(ActionEvent arg0) {
-					update();
-				}
-			});
-
-			quickEntryPanel
-					.add(ComponentUtils
-							.addToFlowLayout(
-									ComponentUtils.addToHorizontalBorderLayout(new JLabel("Enter table number"),
-											roundNumber, ComponentUtils.addToHorizontalBorderLayout(
-													new JLabel("or choose a player"), playerCombo, null)),
-									FlowLayout.CENTER),
-							BorderLayout.NORTH);
-
-			quickEntryPanel.add(quickEntrySubPanel);
-		}
-
-		return quickEntryPanel;
+	public JFXPanel getPanel() {
+		return this.panel;
 	}
 
-	public void update() {
+	public VBox buildPanel() {
 
-		scroll.getViewport().removeAll();
-		scroll.getViewport().add(ComponentUtils.addToFlowLayout(buildPanel(), FlowLayout.CENTER));
-		ComponentUtils.repaint(XWingRoundPanel.this);
-
-		Integer i = null;
-		try {
-			i = Integer.parseInt(roundNumber.getText());
-		} catch (NumberFormatException e) {
-
-		}
-
-		XWingPlayer player = playerCombo.getSelectedIndex() == 0 ? null : (XWingPlayer) playerCombo.getSelectedItem();
-
-		if (player != null) {
-			roundNumber.setEnabled(false);
-		} else if (i != null) {
-			playerCombo.setEnabled(false);
-		} else {
-			roundNumber.setEnabled(true);
-			playerCombo.setEnabled(true);
-		}
-
-		GamePanel gamePanel = null;
-		if (i != null) {
-			if (i > gamePanels.size()) {
-				return;
-			}
-
-			gamePanel = gamePanels.get(i - 1);
-		} else if (player != null) {
-			for (GamePanel g : gamePanels) {
-				if (g.getMatch().getPlayer1() == player) {
-					gamePanel = g;
-					break;
-				} else if (g.getMatch().getPlayer2() != null && g.getMatch().getPlayer2() == player) {
-					gamePanel = g;
-					break;
-				}
-			}
-		}
-
-		if (gamePanel == null) {
-			return;
-		}
-
-		quickEntrySubPanel.add(gamePanel.getPlayerTitle(), BorderLayout.CENTER);
-
-		quickEntrySubPanel.removeAll();
-
-		JPanel panel = new JPanel(new GridBagLayout());
-
-		GridBagConstraints gbc = new GridBagConstraints();
-
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.gridwidth = 2;
-		gbc.anchor = GridBagConstraints.EAST;
-		panel.add(gamePanel.getPlayerTitle(), gbc);
-
-		gbc.gridx = 2;
-		gbc.fill = GridBagConstraints.BOTH;
-		panel.add(gamePanel.getResultCombo(), gbc);
-
-		if (gamePanel.getMatch().getPlayer2() != null) {
-			gbc.gridy++;
-			gbc.gridwidth = 1;
-			gbc.fill = GridBagConstraints.NONE;
-
-			gbc.gridx = 1;
-			panel.add(gamePanel.getPlayer1KillLabel(), gbc);
-
-			gbc.gridx = 2;
-			panel.add(gamePanel.getPlayer1KillPointsField(), gbc);
-
-			gbc.gridy++;
-			gbc.gridwidth = 1;
-
-			gbc.gridx = 1;
-			panel.add(gamePanel.getPlayer2KillLabel(), gbc);
-
-			gbc.gridx = 2;
-			panel.add(gamePanel.getPlayer2KillPointsField(), gbc);
-		}
-
-		quickEntrySubPanel.add(panel, BorderLayout.CENTER);
-
-		ComponentUtils.repaint(quickEntrySubPanel);
-		ComponentUtils.repaint(quickEntryPanel);
-	}
-
-	public JPanel buildPanel() {
-
-		JPanel panel = new JPanel(new GridBagLayout());
-
-		GridBagConstraints gbc = new GridBagConstraints();
-
-		gbc.gridx = 0;
-		gbc.gridy = -1;
-
+		var root = new Bootstrap().VBox();
+		root.setPadding(new Insets(Bootstrap.SPACING * 2));
 		for (GamePanel gp : gamePanels) {
-			gbc.gridy++;
-			gbc.gridx = 0;
-			gbc.gridwidth = 2;
-			gbc.anchor = GridBagConstraints.EAST;
-			panel.add(gp.getPlayerTitle(), gbc);
-
-			gbc.gridx = 2;
-			gbc.fill = GridBagConstraints.BOTH;
-			panel.add(gp.getResultCombo(), gbc);
+			root.getChildren().add(gp.getPlayerTitle());
+			root.getChildren().add(gp.getResultCombo());
 
 			if (gp.getMatch().getPlayer2() != null) {
-				gbc.gridy++;
-				gbc.gridwidth = 1;
-				gbc.fill = GridBagConstraints.NONE;
-
-				gbc.gridx = 1;
-				panel.add(gp.getPlayer1KillLabel(), gbc);
-
-				gbc.gridx = 2;
-				panel.add(gp.getPlayer1KillPointsField(), gbc);
-
-				gbc.gridx = 3;
-				gbc.anchor = GridBagConstraints.WEST;
-				panel.add(gp.getPlayer1KillPointsField().getIndicator(), gbc);
-
-				gbc.gridy++;
-				gbc.gridwidth = 1;
-				gbc.anchor = GridBagConstraints.EAST;
-
-				gbc.gridx = 1;
-				panel.add(gp.getPlayer2KillLabel(), gbc);
-
-				gbc.gridx = 2;
-				panel.add(gp.getPlayer2KillPointsField(), gbc);
-
-				gbc.gridx = 3;
-				gbc.anchor = GridBagConstraints.WEST;
-				panel.add(gp.getPlayer2KillPointsField().getIndicator(), gbc);
+				root.getChildren().add(gp.getPlayer1KillLabel());
+				root.getChildren().add(gp.getPlayer1KillPointsField());
+				root.getChildren().add(gp.getPlayer2KillLabel());
+				root.getChildren().add(gp.getPlayer2KillPointsField());
 			}
 		}
-
-		return panel;
+		return root;
 	}
 
 	public List<XWingMatch> getMatches() {
 		return matches;
 	}
 
-	public void resetGamePanels(boolean isTextOnly) {
-		for (GamePanel gp : gamePanels) {
-			gp.reset(isTextOnly);
-		}
-		getQuickEntryPanel().setVisible(CryodexController.getOptions().isShowQuickFind());
-		ComponentUtils.repaint(this);
-	}
-
 	private class GamePanel {
 		private final XWingMatch match;
-		private JLabel playersTitle;
-		private JComboBox<String> resultsCombo;
-		private ConfirmationTextField player1KillPoints;
-		private ConfirmationTextField player2KillPoints;
-		private JLabel player1KillLabel;
-		private JLabel player2KillLabel;
-		private boolean isLoading = false;
-		private int tableNumber = -1;
+		private Label playersTitle;
+		private ComboBox<String> resultsCombo;
+		private TextField player1KillPoints;
+		private TextField player2KillPoints;
+		private Label player1KillLabel;
+		private Label player2KillLabel;
+		private boolean isLoading;
+		private final int tableNumber;
 
 		public GamePanel(int tableNumber, XWingMatch match) {
-
 			this.tableNumber = tableNumber;
-
-			isLoading = true;
-
 			this.match = match;
-
-			reset(false);
-
+			isLoading = true;
 			init();
 		}
 
 		private void init() {
 			if (match.isMatchComplete()) {
 				if (match.isBye()) {
-					getResultCombo().setSelectedIndex(1);
+					getResultCombo().getSelectionModel().select(1);
 				} else {
 					if (match.getWinner() == match.getPlayer1()) {
-						getResultCombo().setSelectedIndex(1);
+						getResultCombo().getSelectionModel().select(1);
 					} else if (match.getWinner() == match.getPlayer2()) {
-						getResultCombo().setSelectedIndex(2);
+						getResultCombo().getSelectionModel().select(2);
 					}
 				}
 			}
@@ -345,77 +109,60 @@ public class XWingRoundPanel extends JPanel {
 			return match;
 		}
 
-		private JLabel getPlayerTitle() {
+		private Label getPlayerTitle() {
 			if (playersTitle == null) {
-
-				playersTitle = new JLabel("");
-				playersTitle.setFont(new Font(playersTitle.getFont().getName(), playersTitle.getFont().getStyle(), 20));
-				playersTitle.setHorizontalAlignment(SwingConstants.RIGHT);
+				playersTitle = new Bootstrap().H5("");
 			}
 			return playersTitle;
 		}
 
-		private JLabel getPlayer1KillLabel() {
+		private Label getPlayer1KillLabel() {
 			if (player1KillLabel == null) {
-				player1KillLabel = new JLabel();
+				player1KillLabel = new Bootstrap().H5("");
 			}
 			return player1KillLabel;
 		}
 
-		private JLabel getPlayer2KillLabel() {
+		private Label getPlayer2KillLabel() {
 			if (player2KillLabel == null) {
-				player2KillLabel = new JLabel();
+				player2KillLabel = new Bootstrap().H5("");
 			}
 			return player2KillLabel;
 		}
 
 		private String[] getComboValues() {
-
 			if (match.getPlayer2() == null) {
-                String[] values = {"Select a result", "BYE"};
-                return values;
+				return new String[]{"Select a result", "BYE"};
             } else {
-				String generic = XWingModule.getInstance().getOptions().isEnterOnlyPoints() ? "Enter results"
-						: "Select a result";
-				String[] values = { generic, "WIN - " + match.getPlayer1().getName(),
-						"WIN - " + match.getPlayer2().getName(), "Draw" };
-				return values;
+				String generic = XWingModule.getInstance().getOptions().isEnterOnlyPoints()
+					? "Enter results"
+					: "Select a result";
+				return new String[]{
+					generic,
+					"WIN - " + match.getPlayer1().getName(),
+					"WIN - " + match.getPlayer2().getName(),
+					"Draw"
+				};
 			}
 		}
 
-		private JComboBox<String> getResultCombo() {
+		private ComboBox<String> getResultCombo() {
 			if (resultsCombo == null) {
-
-				resultsCombo = new JComboBox<String>(getComboValues());
-
-				resultsCombo.setRenderer(new DefaultListCellRenderer() {
-					private static final long serialVersionUID = 1L;
-
-					@Override
-					public void paint(Graphics g) {
-						setForeground(Color.BLACK);
-						super.paint(g);
-					}
-				});
-
-				resultsCombo.addActionListener(new ActionListener() {
-
-					@Override
-					public void actionPerformed(ActionEvent arg0) {
-						comboChange();
-					}
-				});
+				resultsCombo = new ComboBox<>();
+				resultsCombo.getItems().addAll(getComboValues());
+				resultsCombo
+					.getSelectionModel()
+					.selectedItemProperty()
+					.addListener((options, oldValue, newValue) -> comboChange());
 			}
 			return resultsCombo;
 		}
 
 		private void comboChange() {
-
 			if (isLoading) {
 				return;
 			}
-
-			switch (resultsCombo.getSelectedIndex()) {
+			switch (resultsCombo.getSelectionModel().getSelectedIndex()) {
 			case 0:
 				match.setWinner(null);
 				match.setBye(false);
@@ -440,163 +187,83 @@ public class XWingRoundPanel extends JPanel {
 
 		}
 
-		public ConfirmationTextField getPlayer1KillPointsField() {
+		public TextField getPlayer1KillPointsField() {
 			if (player1KillPoints == null) {
-				player1KillPoints = new ConfirmationTextField();
-				player1KillPoints.addFocusListener(new FocusListener() {
-
-					@Override
-					public void focusLost(FocusEvent arg0) {
+				player1KillPoints = new TextField();
+				player1KillPoints
+					.textProperty()
+					.addListener((options, oldValue, newValue) -> {
 						Integer points = null;
 						try {
 							points = Integer.valueOf(player1KillPoints.getText());
-						} catch (Exception e) {
-
-						}
-
+						} catch (Exception ignored) {}
 						match.setPlayer1PointsDestroyed(points);
 						tournament.getTournamentGUI().getRankingTable().resetPlayers();
-
-						if (XWingModule.getInstance().getOptions().isEnterOnlyPoints()) {
-							setResultsCombo();
-						}
+						setResultsCombo();
 					}
-
-					@Override
-					public void focusGained(FocusEvent arg0) {
-                        focusLost(arg0);
-					}
-				});
-				ComponentUtils.forceSize(player1KillPoints, 50, 25);
+				);
 			}
-
 			return player1KillPoints;
 		}
 
-		public ConfirmationTextField getPlayer2KillPointsField() {
+		public TextField getPlayer2KillPointsField() {
 			if (player2KillPoints == null) {
-				player2KillPoints = new ConfirmationTextField();
-				player2KillPoints.addFocusListener(new FocusListener() {
-
-					@Override
-					public void focusLost(FocusEvent arg0) {
-						Integer points = null;
-						try {
-							points = Integer.valueOf(player2KillPoints.getText());
-						} catch (Exception e) {
-
-						}
-
-						match.setPlayer2PointsDestroyed(points);
-						tournament.getTournamentGUI().getRankingTable().resetPlayers();
-
-						if (XWingModule.getInstance().getOptions().isEnterOnlyPoints()) {
+				player2KillPoints = new TextField();
+				player2KillPoints
+					.textProperty()
+					.addListener(
+						(options, oldValue, newValue) -> {
+							Integer points = null;
+							try {
+								points = Integer.valueOf(player2KillPoints.getText());
+							} catch (Exception ignored) {}
+							match.setPlayer1PointsDestroyed(points);
+							tournament.getTournamentGUI().getRankingTable().resetPlayers();
 							setResultsCombo();
 						}
-					}
-
-					@Override
-					public void focusGained(FocusEvent arg0) {
-                        focusLost(arg0);
-					}
-				});
-				ComponentUtils.forceSize(player2KillPoints, 50, 25);
+					);
 			}
-
 			return player2KillPoints;
-		}
-
-		public void reset(boolean isTextOnly) {
-
-			String titleText = null;
-
-			boolean showKillPoints = XWingModule.getInstance().getOptions().isShowKillPoints();
-			boolean enterOnlyPoints = XWingModule.getInstance().getOptions().isEnterOnlyPoints();
-
-			getPlayer1KillLabel().setVisible(showKillPoints);
-			getPlayer1KillPointsField().setVisible(showKillPoints);
-			getPlayer2KillLabel().setVisible(showKillPoints);
-			getPlayer2KillPointsField().setVisible(showKillPoints);
-
-			if (match.getPlayer2() == null) {
-				titleText = match.getPlayer1().getName() + " has a BYE";
-			} else {
-				titleText = match.getPlayer1().getName() + " VS " + match.getPlayer2().getName();
-				if (match.isDuplicate()) {
-					titleText = "(Duplicate)" + titleText;
-				}
-
-				if (CryodexController.getOptions().isShowTableNumbers()) {
-					titleText = tableNumber + ": " + titleText;
-				}
-
-				getPlayer1KillLabel().setText(match.getPlayer1().getName() + " kill points");
-				getPlayer2KillLabel().setText(match.getPlayer2().getName() + " kill points");
-			}
-
-			getPlayerTitle().setText(titleText);
-
-			if (isTextOnly == false) {
-				getResultCombo().removeAllItems();
-				for (String s : getComboValues()) {
-					getResultCombo().addItem(s);
-				}
-			}
-
-			getResultCombo().setEnabled(!enterOnlyPoints);
-
-			if (enterOnlyPoints) {
-				if (getComboValues().length == 2) {
-					getResultCombo().setSelectedIndex(1);
-					match.setBye(true);
-				} else {
-					setResultsCombo();
-				}
-			}
 		}
 
 		/**
 		 * This function sets the combo box value to the winner of the match based on points.
 		 */
 		public void setResultsCombo() {
-
 			boolean enterOnlyPoints = XWingModule.getInstance().getOptions().isEnterOnlyPoints();
-
 			if (match.getPlayer1PointsDestroyed() != null || match.getPlayer2PointsDestroyed() != null) {
-
 				Integer p1points = match.getPlayer1PointsDestroyed() == null ? 0 : match.getPlayer1PointsDestroyed();
 				Integer p2points = match.getPlayer2PointsDestroyed() == null ? 0 : match.getPlayer2PointsDestroyed();
-
 				if (p1points.equals(p2points)) {
-					// Only reset the result if it was not enabled before. This
-					// prevents the combo box from resetting if the result
-					// didn't actually change.
-					if (getResultCombo().isEnabled() == false) {
-						getResultCombo().setSelectedIndex(0);
+					if (!getResultCombo().isEditable()) {
+						getResultCombo().getSelectionModel().select(0);
 					}
-					getResultCombo().setEnabled(true);
+					getResultCombo().setEditable(true);
 				}
 				if (p1points > p2points) {
-					getResultCombo().setSelectedIndex(1);
-					getResultCombo().setEnabled(!enterOnlyPoints);
+					getResultCombo().getSelectionModel().select(1);
+					getResultCombo().setEditable(!enterOnlyPoints);
 				}
-
 				if (p2points > p1points) {
-					getResultCombo().setSelectedIndex(2);
-					getResultCombo().setEnabled(!enterOnlyPoints);
+					getResultCombo().getSelectionModel().select(2);
+					getResultCombo().setEditable(!enterOnlyPoints);
+				}
+				if (p2points.equals(p1points)) {
+					getResultCombo().getSelectionModel().select(3);
+					getResultCombo().setEditable(!enterOnlyPoints);
 				}
 			} else {
-				getResultCombo().setSelectedIndex(0);
-				getResultCombo().setEnabled(!enterOnlyPoints);
+				getResultCombo().getSelectionModel().select(0);
+				getResultCombo().setEditable(!enterOnlyPoints);
 			}
 			comboChange();
 		}
 
 		public void markInvalid(boolean isSingleElimination) {
-			if (match.isValidResult(isSingleElimination) == false) {
-				getPlayerTitle().setForeground(Color.red);
+			if (!match.isValidResult(isSingleElimination)) {
+				getPlayerTitle().setTextFill(Color.RED);
 			} else {
-				getPlayerTitle().setForeground(Color.black);
+				getPlayerTitle().setTextFill(Color.BLACK);
 			}
 		}
 	}
